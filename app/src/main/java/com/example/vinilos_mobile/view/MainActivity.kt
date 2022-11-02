406fa676eb13cf40727f0ff2f809a8620461b7c4
Hector Tenezaca <h.tenezaca@uniandes.edu.co>
2022-11-01 22:51:02 -0500

    refactor: AlbumActivity to MainActivity

--- app/src/main/java/com/example/vinilos_mobile/view/MainActivity.kt
+++ app/src/main/java/com/example/vinilos_mobile/view/MainActivity.kt
@@ -6,7 +6,7 @@ import android.util.Log
 import com.example.vinilos_mobile.R
 import com.example.vinilos_mobile.model.repository.VinilosRepository
 
-class AlbumActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_album)
