aa4553ad5307269853a9f5d9d9103496fc78bdfb
Javier López <graulopezjavier@gmail.com>
2022-11-05 19:01:26 -0500

    feat: album detail initial implementation

--- app/src/main/java/com/example/vinilos_mobile/view/AlbumsAdapter.kt
+++ app/src/main/java/com/example/vinilos_mobile/view/AlbumsAdapter.kt
@@ -3,9 +3,14 @@ package com.example.vinilos_mobile.view
 
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.vinilos_mobile.R
@@ -46,6 +51,7 @@ class AlbumsAdapter : RecyclerView.Adapter<AlbumsAdapter.AlbumViewHolder>() {
         Log.d("AlbumsAdapter", "onBindViewHolder: ${albums[position].name}")
         Glide.with(holder.viewDataBinding.root)
             .load(albums[position].cover)
+            .diskCacheStrategy(com.bumptech.glide.load.engine.DiskCacheStrategy.AUTOMATIC)
             .into(holder.viewDataBinding.imageView)
 
         //seetup view when sreen is rotated
@@ -59,9 +65,13 @@ class AlbumsAdapter : RecyclerView.Adapter<AlbumsAdapter.AlbumViewHolder>() {
         }
 
         holder.viewDataBinding.root.setOnClickListener {
-            // val action =
-            // Navigate using that action
-            // holder.viewDataBinding.root.findNavController().navigate(action)
+            val action = AlbumListFragmentDirections.superexplosion()
+            // Use the fragment manager to replace the main fragment view
+            val fm = (holder.itemView.context as FragmentActivity).supportFragmentManager
+            val fragment = AlbumDetailFragment.newInstance(albumId = albums[position].albumId)
+            // Add the current fragment to the back stack
+            fm.beginTransaction().replace(R.id.fragment_container_view, fragment)
+                .addToBackStack("Album List").commit()
         }
     }
 
