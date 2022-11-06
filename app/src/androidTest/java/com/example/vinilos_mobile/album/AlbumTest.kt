2088070bd6fecd24ca80c0234ae6fd83fbbc836d
Javier López <graulopezjavier@gmail.com>
2022-11-06 18:53:56 -0500

    test: album detail, and artist list

--- app/src/androidTest/java/com/example/vinilos_mobile/album/AlbumTest.kt
+++ app/src/androidTest/java/com/example/vinilos_mobile/album/AlbumTest.kt
@@ -1,18 +1,20 @@
 package com.example.vinilos_mobile.album
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.ViewAssertion
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
+import org.hamcrest.Matchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
-import com.example.vinilos_mobile.view.WelcomeActivity
+import com.example.vinilos_mobile.view.*
 import com.example.vinilos_mobile.R
-import org.hamcrest.Matchers.*
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.util.regex.Matcher
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
@@ -27,30 +29,31 @@ class AlbumTest {
         val guestButton = onView(withId(R.id.userIsGuestButton))
         guestButton.perform(click())
 
-        // alternativa 1
-        // TODO: evitar sleep
         Thread.sleep(1000)
-
         val albumRecyclerView = onView(withId(R.id.album_recycler_view))
         albumRecyclerView.perform(click())
-        albumRecyclerView.check(matches(hasDescendant(withId(R.id.album_name))))
-        albumRecyclerView.check(matches(hasDescendant(withId(R.id.album_image_view))))
+        albumRecyclerView.check(matches(hasDescendant(withId(R.id.album_list_album_name))))
+    }
 
-        // se comprueba lectura satisfactoria ya que al ejecutar la siguiente
-        // linea, arroja error por 'matches multiple views in the hierarchy'
-        //onView(withId(R.id.album_name)).check(matches(isDisplayed()))
+    @Test
+    fun detailAlbum() {
 
-        /*// alternativa 2
-        // requiere: import androidx.test.espresso.contrib.RecyclerViewActions
+        val guestButton = onView(withId(R.id.userIsGuestButton))
+        guestButton.perform(click())
+
         Thread.sleep(1000)
-
-        val albumRecyclerView = onView(ViewMatchers.withId(R.id.album_recycler_view))
-            .perform(
-                RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(0)
-            )
-
-        albumRecyclerView.check(matches(hasDescendant(withId(R.id.album_name))))
-        albumRecyclerView.check(matches(hasDescendant(withId(R.id.album_image_view))))*/
-
+        val albumRecyclerView = onView(withId(R.id.album_recycler_view))
+        albumRecyclerView.perform(click())
+        albumRecyclerView.check(matches(hasDescendant(withId(R.id.album_list_album_name))))
+        // There are multiple items with the same, id pick them all, and click the first one
+        onView(allOf(withId(R.id.album_list_album_name), withText("Buscando América"))).perform(
+            click()
+        );
+        // Check for the title
+        onView(withText("Buscando América")).check(matches(isDisplayed()))
+        // Check for the description
+        onView(withId(R.id.album_description_content)).check(matches(isDisplayed()))
+        onView(withId(R.id.album_year)).check(matches(isDisplayed()))
+        onView(withId(R.id.album_genre)).check(matches(isDisplayed()))
     }
 }
