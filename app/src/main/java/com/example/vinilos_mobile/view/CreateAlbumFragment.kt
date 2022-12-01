66ca4fb24937114ca1312dd3a78ab78bce5bec2e
miso-alejosaur <a.santamaria11@uniandes.edu.co>
2022-11-30 20:11:19 -0500

    validations and accesibility

--- app/src/main/java/com/example/vinilos_mobile/view/CreateAlbumFragment.kt
+++ app/src/main/java/com/example/vinilos_mobile/view/CreateAlbumFragment.kt
@@ -1,24 +1,29 @@
 package com.example.vinilos_mobile.view
 
-import android.app.Activity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.findNavController
 import com.example.vinilos_mobile.R
+import com.example.vinilos_mobile.databinding.FragmentCreateAlbumBinding
 import com.example.vinilos_mobile.model.models.Album
 import com.example.vinilos_mobile.viewmodel.AlbumViewModel
 import com.google.android.material.button.MaterialButton
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
 class CreateAlbumFragment : Fragment(), View.OnClickListener {
-
+    lateinit var binding: FragmentCreateAlbumBinding
     private lateinit var viewModel: AlbumViewModel
 
+    private lateinit var nameEdit: TextInputEditText
+    private lateinit var imageEdit: TextInputEditText
+    private lateinit var dateEdit: TextInputEditText
+    private lateinit var descriptionEdit: TextInputEditText
     private lateinit var name: TextInputLayout
     private lateinit var image: TextInputLayout
     private lateinit var date: TextInputLayout
@@ -29,14 +34,66 @@ class CreateAlbumFragment : Fragment(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentCreateAlbumBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_create_album, container, false)
+    }
+
+    private fun setupListeners(view: View) {
+        nameEdit.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                validateAlbumName(p0.toString())
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        })
+
+        imageEdit.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                validateAlbumImage(p0.toString())
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        })
+
+        dateEdit.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                validateAlbumDate(p0.toString())
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        })
+
+        descriptionEdit.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                validateAlbumDescription(p0.toString())
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        })
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -48,12 +105,19 @@ class CreateAlbumFragment : Fragment(), View.OnClickListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
+        nameEdit = view.findViewById<TextInputEditText>(R.id.album_name_edit)
+        imageEdit = view.findViewById<TextInputEditText>(R.id.album_image_edit)
+        dateEdit = view.findViewById<TextInputEditText>(R.id.album_date_edit)
+        descriptionEdit = view.findViewById<TextInputEditText>(R.id.album_description_edit)
+
         name = view.findViewById<TextInputLayout>(R.id.new_album_name)
         image = view.findViewById<TextInputLayout>(R.id.new_album_image)
         date = view.findViewById<TextInputLayout>(R.id.new_album_date)
         description = view.findViewById<TextInputLayout>(R.id.new_album_description)
         genre = view.findViewById<TextInputLayout>(R.id.new_album_gender)
         label = view.findViewById<TextInputLayout>(R.id.new_album_label)
+
+        setupListeners(view)
 
         val btn: MaterialButton = view.findViewById(R.id.buttonpost)
         btn.setOnClickListener(this)
@@ -88,9 +152,52 @@ class CreateAlbumFragment : Fragment(), View.OnClickListener {
                 requireActivity().supportFragmentManager.popBackStack()
             }
             R.id.cancel_button -> {
-
                 requireActivity().supportFragmentManager.popBackStack()
             }
         }
+    }
+
+    private fun validateAlbumName(text: String): Boolean {
+        if(text.trim().isEmpty()){
+            name.error = "Required"
+            name.requestFocus()
+            return false
+        }
+        name.error = null
+        name.clearFocus()
+        return true
+    }
+
+    private fun validateAlbumImage(text: String): Boolean {
+        if(text.trim().isEmpty()){
+            image.error = "Required"
+            image.requestFocus()
+            return false
+        }
+        image.error = null
+        image.clearFocus()
+        return true
+    }
+
+    private fun validateAlbumDate(text: String): Boolean {
+        if(text.trim().isEmpty()){
+            date.error = "Required"
+            date.requestFocus()
+            return false
+        }
+        date.error = null
+        date.clearFocus()
+        return true
+    }
+
+    private fun validateAlbumDescription(text: String): Boolean {
+        if(text.trim().isEmpty()){
+            description.error = "Required"
+            description.requestFocus()
+            return false
+        }
+        description.error = null
+        description.clearFocus()
+        return true
     }
 }
